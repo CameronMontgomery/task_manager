345f3b7ded6eadb246502ffc6b3acd53e37d9254
Cameron Montgomery <66389466+Tribalash@users.noreply.github.com>
2022-01-05 14:38:07 -0700

    added endpoints for updating users and tasks

--- src/index.js
+++ src/index.js
@@ -44,6 +44,29 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send('Invalid Update Information')
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  } 
+})
+
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
@@ -57,8 +80,8 @@ app.post("/tasks", async (req, res) => {
 
 app.get("/tasks", async (req, res) => {
   try {
-    const task = await Task.find({})
-    res.send(task)
+    const tasks = await Task.find({})
+    res.send(tasks)
   } catch (e) {
     res.status(500).send()
   }
@@ -78,6 +101,29 @@ app.get("/tasks/:id", async (req, res) => {
     res.status(500).send()
   }
 });
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send('Invalid Update Information')
+  }
+  
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);

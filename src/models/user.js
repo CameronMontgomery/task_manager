c5dc0cc6cd9a02a6c1da87a9fe20ad05b06e21f6
Cameron Montgomery <66389466+Tribalash@users.noreply.github.com>
2022-01-07 13:26:46 -0700

    Added a schema for user, modified user update so that middleware will be used, added bcrypt.js package

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
+
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,5 +40,15 @@ const User = mongoose.model('User', {
     }
   }
 })
+
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  console.log('Just before saving')
+
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- src/routes/user.js
+++ src/routes/user.js
@@ -49,11 +49,15 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    const user = await User.findById(req.params.id)
 
     if (!user) {
       return res.status(404).send()
     }
+
+    updates.forEach((update) => user[update] = req.body[update])
+    await user.save()
 
     res.send(user)
   } catch (e) {
